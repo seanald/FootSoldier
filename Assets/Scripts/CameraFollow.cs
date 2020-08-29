4225d551198c5c98da5f570a6917538218cc07af
seanald <s.michael.mcdonald@gmail.com>
2020-08-29 15:48:42 -0400

    Camera Follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform followTransform;
+
+    private Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mainCamera = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float camY = followTransform.position.y;
+        float camX = followTransform.position.x;
+        this.transform.position = new UnityEngine.Vector3(camX, camY, transform.position.z);
+    }
+}
